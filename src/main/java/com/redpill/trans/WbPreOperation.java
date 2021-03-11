a7dcf9f5eba091bfe7c7a56fd19ef35c887e980b
labigthree <gao750277>
2021-03-11 18:51:56 +0800

    update database to webservice flow and template.

--- src/main/java/com/redpill/trans/WbPreOperation.java
+++ src/main/java/com/redpill/trans/WbPreOperation.java
@@ -13,7 +13,7 @@ public class WbPreOperation implements Callable {
     @Override
     public Object onCall(MuleEventContext muleEventContext) throws Exception {
     	String operationKey = "varOperation";
-    	String paramKey = "one";
+    	String paramKey = "params";
         //test
         String soapRequest = muleEventContext.getMessageAsString();
         LogTool.logInfo(2, "soap request:" + soapRequest);
@@ -23,11 +23,10 @@ public class WbPreOperation implements Callable {
         muleEventContext.getMessage().setInvocationProperty(operationKey, operation);
         String param = soapRequest.split("<"+paramKey+">")[1];
         param = param.split("</"+paramKey+">")[0];
+        System.out.println(param);
         Map<String, Object> map = (Map)JSON.parse(param);
         for(Map.Entry<String, Object> en : map.entrySet()) {
-        	System.out.println(en.getKey() + ":" + en.getValue());
         	muleEventContext.getMessage().setInvocationProperty(en.getKey(), en.getValue());
-        	System.out.println(muleEventContext.getMessage().getInvocationProperty(en.getKey()).toString());
         }
 
         return muleEventContext;
